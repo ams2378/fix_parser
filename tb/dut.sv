149187131162c5246930dc4ec873832de1fd6ab8
Adil Sadik <ams2378@columbia.edu>
2013-02-18 15:36:52 -0500

    edit

--- tb/dut.sv
+++ tb/dut.sv
@@ -1,16 +1,26 @@
+/**
+ * @filename		dut.sv 
+ *
+ * @brief     	        A simple testbench. 	
+ *
+ * @author		Adil Sadik <sadik.adil@gmail.com> 
+ */
+
+
+
 module parser_test();
 
 reg 		clk;
 reg 		rst;
 reg[31:0] 	data_i;
 reg[31:0] 	tag_o;
-reg[31:0] 	body_o;
+reg[31:0] 	value_o;
 reg 		tag_valid_o;
-reg 		body_valid_o;
+reg 		value_valid_o;
 
 bit error;
 
-fix_parser_top dut(.clk, .rst, .data_i, .tag_o, .body_o, .tag_valid_o, .body_valid_o);
+fix_parser_top dut(.clk, .rst, .data_i, .tag_o, .value_o, .tag_valid_o, .value_valid_o);
 
 initial begin
 
