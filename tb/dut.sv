c80fc3a51b1605c9a9ef4eda370346201eff41dd
Adil Sadik <ams2378@columbia.edu>
2013-03-01 02:46:47 -0500

    edit

--- tb/dut.sv
+++ tb/dut.sv
@@ -32,12 +32,13 @@ module parser_test();
 	reg				read_message_i;
 
 	reg[255:0]			output_value_o;
+	reg				output_value_valid_o,
 /*
 fix_parser_top dut(.clk, .rst, .data_i, .tag_o, .value_o, .end_of_body_o, .start_of_header_o, 
 		   .t_wr_cs_o, .t_wr_en_o, .v_wr_cs_o, .v_wr_en_o,  .empty, .full);
 */
 
-fix_parser_top dut(.clk, .rst, .data_i, .find_tag_i, .message_num_i, .read_message_i, .output_value_o, .start_of_header_o, .empty_o, .full_o);
+fix_parser_top dut(.clk, .rst, .data_i, .find_tag_i, .message_num_i, .read_message_i, .output_value_o, .output_value_valid_o , .start_of_header_o, .empty_o, .full_o);
 
 initial begin
 
