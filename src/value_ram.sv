a6324e74484c6f7fcaac12dcb3639bb64aa315f7
Adil Sadik <ams2378@columbia.edu>
2013-03-01 01:35:48 -0500

    edit

--- src/value_ram.sv
+++ src/value_ram.sv
@@ -8,14 +8,13 @@ module value_ram # (parameter ADDR_WIDTH = 5, DATA_WIDTH = 256 ) (
 	input				we_i      	, 		// Write Enable/Read Enable
 	input				oe_i      	, 		// Output Enable
 
-	output logic [DATA_WIDTH-1:0] 		mem [0: (1 << ADDR_WIDTH -1)],
 	output [DATA_WIDTH-1 : 0]     	data_o
 ); 
 
 parameter RAM_DEPTH = 1 << ADDR_WIDTH;
 
 logic [DATA_WIDTH-1:0] 		data_out ; 
-//logic [DATA_WIDTH-1:0] 		mem [0:RAM_DEPTH-1];
+logic [DATA_WIDTH-1:0] 		mem [0:RAM_DEPTH-1];
 logic [ADDR_WIDTH-1:0] 		wr_pointer;
 
 always_ff @ (posedge clk or posedge rst) begin
