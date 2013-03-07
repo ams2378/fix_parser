5d4fa9ea9b241c285aa9ab6945f1744e42f6a7b1
Adil Sadik <ams2378@columbia.edu>
2013-03-06 22:31:44 -0500

    edit

--- src/checksum.sv
+++ src/checksum.sv
@@ -59,7 +59,7 @@ always_ff @(state or start_i or end_i or data_i) begin
 				if (end_i != 1) begin
 					temp = 8'd118 + data_i;
 					if (temp > 9d'256) begin
-						temp = temp % 9d'256;
+						temp = temp % 9'd256;
 					end
 					next_state = state1;
 				end else begin
