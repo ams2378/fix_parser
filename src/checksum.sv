5a07ee7e9a4bb6c0d42e9587b753bc49e000dede
Adil Sadik <ams2378@columbia.edu>
2013-03-06 23:45:38 -0500

    edit

--- src/checksum.sv
+++ src/checksum.sv
@@ -40,7 +40,7 @@ always_ff @(posedge clk) begin
 end
 
 //always_ff @(clk or state or start_i or end_i or data_i) begin
-always_ff @(posedge clk) begin
+always_ff @(state or start_i or end_i or data_i) begin
 
 	if (rst) begin
 		checksum = '0;
