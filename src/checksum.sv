1eaf147f4b350b2f10f900b9c8c29dbb3e8b4584
Adil Sadik <ams2378@columbia.edu>
2013-03-07 20:01:32 -0500

    edit

--- src/checksum.sv
+++ src/checksum.sv
@@ -20,18 +20,20 @@ module checksum(
 	output[7:0]		checksum_o
 );
 
-parameter 			state0 = 2'b00;
-parameter			state1 = 2'b01;
-parameter			state2 = 2'b10;
-parameter			state3 = 2'b11;
+parameter 			state0 = 3'b000;
+parameter			state1 = 3'b001;
+parameter			state2 = 3'b010;
+parameter			state3 = 3'b011;
+parameter			state4 = 3'b100;
 
-logic [1:0]			state;
-logic [1:0]			next_state;
+logic [2:0]			state;
+logic [2:0]			next_state;
 logic [7:0]			checksum;
 logic [8:0]			temp;
 logic [7:0]			soh = 8'h01;
 logic [7:0]			head = 8'h38;
 
+logic [7:0]			chksm_value;
 
 always_ff @(posedge clk) begin
 
@@ -43,6 +45,7 @@ always_ff @(state or start_i or end_i) begin
 
 	if (rst) begin
 		checksum = '0;
+		chksm_value = '0;
 	end
 
 	case(state) 
@@ -90,5 +93,18 @@ always_ff @(state or start_i or end_i) begin
 end
 
 assign checksum_o = checksum; 
+
+
+ascii2int ascii2binary (
+
+		.clk, 
+		.rst,
+		
+		.data_i,
+		.end_i,
+		.result_o(chksm_value)
+);
+
+
 
 endmodule
